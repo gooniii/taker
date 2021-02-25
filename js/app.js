da7ed56467e56e3372b22cc6eda762e3ab97744a
lee hyegoon <hegun_i@colgis.co.jp>
2021-02-25 15:57:29 +0900

    업데이트

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-const tl = gsap.timeline({ defaults : { ease: "power1.out" } });
+const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
 
-tl.to(".text", { y: "0%", duration: 1, stagger: 0.5 });
-tl.to(".intro", { y: "-100%", duration: 1});
-tl.fromTo(".img", { opacity : 0 }, { opacity:1, duration:1});
-tl.fromTo(".title", { opacity : 0 }, { opacity:1, duration:1}, "-=1");
-tl.fromTo(".items", { opacity : 0 }, { y: -10, opacity:1, duration:1});
+tl.to('.text', { y: '0%', duration: 1, stagger: 0.7 });
+tl.to('.intro', { y: '-100%', duration: 1 });
+tl.fromTo('.img', { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo('.title', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
+tl.fromTo('.items', { opacity: 0 }, { y: -10, opacity: 1, duration: 1 });
